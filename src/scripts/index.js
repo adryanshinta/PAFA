0bd227dc0912dbb95c43b945951021275bab10b4
Jacky Andrazat <jackyandrazat@gmail.com>
2022-11-21 03:11:15 +0700

    Create App Shell + Config Drawer
    Membuat App Shel dengan  menambahkan file index.html style css dan resposive dan index js,
    
    untuk Config drawer disini saya menambahkan file app dan drawer initiator untuk menampilkan fungsi touch dan sedikit edit index js.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,11 @@
+/* eslint-disable no-unused-vars */
 import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/responsive.css';
+import App from './views/app';
 
-console.log('Hello Coders!');
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
