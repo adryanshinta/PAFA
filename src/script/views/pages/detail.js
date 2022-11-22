de06fb52563e6ec66ad75dbe569a96071494992d
icanurpramanti <icanurpramanti2@gmail.com>
2022-11-22 23:08:18 +0700

    menambahkan detail gambar menu orgtua dll
    menambahkan codingan detail  gambar menu orangtua smpai ibu muda dan membuat box artikel

---
+++ src/script/views/pages/detail.js
@@ -0,0 +1,92 @@
+/* eslint-disable no-tabs */
+const Beranda = {
+    async render() {
+      return `
+      <article class="data">
+     
+      <article id="orangtua" class="card">
+                  <h2 class="tagline">Orangtua</h2>
+                  <img src="../images/heros/orangtua.jpg");
+                  class="featured-image" alt="orangtua">
+      </article>
+      
+      <article id="anak" class="card">
+                  <h2 class="tagline">Anak</h2>
+                  <img src="../images/heros/anak.jpg");
+                  class="featured-image" alt="orangtua">
+      </article>
+  
+      
+      <article id="ibumuda" class="card">
+                  <h2 class="tagline">Ibu Muda</h2>
+                  <img src="../images/heros/orangtua.jpg");
+                  class="featured-image" alt="ibumuda">
+      </article>
+      
+      </article>
+  
+      <article class="utama">
+      <h1 class="utama-label">Artikel Terbaru</h1>
+      </article>
+  
+      <article class="main-content">
+  
+      <div class="content-awal">
+       <p>Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak. </p> 
+      </div>
+  
+      <div class="content-awal">
+      <p>Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+      visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+      visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+      visual seperti font, tipografi, dan tata letak. </p> 
+        </div>
+  
+       <div class="content-awal">
+       <p>Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak. </p> 
+       </div>
+  
+       <div class="content-awal">
+       <p>Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak. </p> 
+      </div>
+  
+      <div class="content-awal">
+      <p>Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+      visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+      visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+      visual seperti font, tipografi, dan tata letak. </p>  
+        </div>
+  
+       <div class="content-awal"> 
+       <p>Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak.Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi 
+       visual seperti font, tipografi, dan tata letak. </p> 
+       </div>
+       
+      </div>
+  
+  
+      </article>
+  
+      
+      
+        `;
+    },
+  
+    async afterRender() {
+      // Fungsi ini akan dipanggil setelah render()
+    },
+  };
+  
+  export default Beranda;
+  
