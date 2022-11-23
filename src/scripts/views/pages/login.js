15c4a10a8bebb7780900e14ccd659c7c435447b0
Jacky Andrazat <jackyandrazat@gmail.com>
2022-11-23 19:40:13 +0700

    Editing Front End
    Mengedit beberapa tampilan yang di rubah

--- src/scripts/views/pages/login.js
+++ src/scripts/views/pages/login.js
@@ -2,6 +2,16 @@ const Login = {
   async render() {
     return `
           <h2>Login Page</h2>
+      <div class="container">
+        <h1>Login</h1>
+        <form>
+            <label>Username</label><br>
+            <input type="text"><br>
+            <label>Password</label><br>
+            <input type="password"><br>
+            <button>Log in</button>
+        </form>
+      </div>
         `;
   },
 
